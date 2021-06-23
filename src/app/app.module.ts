f627e9b3b3be6f01f83f3d3b706ddf910fbb01f3
Martin Guo <h66guo@uwaterloo.ca>
2021-06-23 03:34:28 -0400

    step 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import { AngularFireStorageModule } from '@angular/fire/storage'
 import { AngularFireAuthModule } from '@angular/fire/auth'
 import { environment } from '../environments/environment'
 import { LoginComponent } from './login/login.component'
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button'
+import {fabric} from 'fabric'
+// import { Login1Component } from './login1/login1.component'
 
 const config = {
   apiKey: "AIzaSyBeS6jhQcIj4Ao9IDv9TvYJF76TvrLc_dM",
@@ -28,7 +32,9 @@ const config = {
     AngularFireModule.initializeApp(config),
     AngularFirestoreModule, // firestore
     AngularFireAuthModule, // auth
-    AngularFireStorageModule // storage
+
+    AngularFireStorageModule, BrowserAnimationsModule ,// storage
+    MatButtonModule
   ],
   declarations: [AppComponent ,LoginComponent],
   bootstrap: [ AppComponent ]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,50 @@
 import { Component } from '@angular/core'
 import { AngularFireAuth } from '@angular/fire/auth'
 import firebase from 'firebase/app'
+import {Router} from '@angular/router'
+import { Observable } from 'rxjs'
 
 @Component({
     selector: 'login', 
 	templateUrl: './login.component.html'
 })
 export class LoginComponent {
+    public user: firebase.User;
+    public authState$: Observable<firebase.User|null>
     constructor(
-		public afAuth: AngularFireAuth
-	) {}
+		public afAuth: AngularFireAuth,
+        private router:Router
+	) {
+
+        this.user= null as any;
+        this.authState$=afAuth.authState;
+        this.authState$.subscribe((user)=>{
+            console.log("I'm getting userrrr");
+            console.log(user?.displayName);
+
+            if(user!==null){
+                this.user=user;
+                // window.location.href='http://fabricjs.com/';
+            }
+        });
+    }
 	login() {
-	  this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+	  this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    //   this.afAuth.getRedirectResult().then(result=>{
+    //     // this.isLoggedIn();
+    //       if(result.user){
+              
+    //           console.log("I should redirect");
+    //           window.location.href='http://fabricjs.com/';
+    //       }
+    //   });
+      
+      
 	}
 	logout() {
 	  this.afAuth.signOut()
 	}
+    isLoggedIn(): boolean{
+        return this.user!==null;
+    }
 }
